f5af46c8c2d87f7463c6dbe1b8934d913e193733
EduPC <edugbuz343@gmail.com>
2023-04-30 17:22:29 +0100

    Pre TypeORM

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,8 +3,8 @@ import { ConfigService } from '@nestjs/config/dist';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient{
-  constructor(config: ConfigService){
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
